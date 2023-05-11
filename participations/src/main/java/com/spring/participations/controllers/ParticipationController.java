a66090141888632e67f0c47d1d43dc708a0fce41
Adrien <39885692+1-irdA@users.noreply.github.com>
2023-05-11 22:54:51 +0200

    Update controller

--- participations/src/main/java/com/spring/participations/controllers/ParticipationController.java
+++ participations/src/main/java/com/spring/participations/controllers/ParticipationController.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/participations")
 public class ParticipationController {
 
     @Autowired
