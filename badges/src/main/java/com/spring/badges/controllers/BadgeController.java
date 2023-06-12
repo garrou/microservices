e6d235fc002d4bc73ef32543509dd3b8b0748006
Carlos-Correia <103515614+Carlos-Correia@users.noreply.github.com>
2023-06-12 14:39:48 +0200

    features/prometheus (#25)
    * feat zipkin
    
    * feat prometheus
    
    * remove logs
    
    ---------
    
    Co-authored-by: Adrien <39885692+garrou@users.noreply.github.com>

--- badges/src/main/java/com/spring/badges/controllers/BadgeController.java
+++ badges/src/main/java/com/spring/badges/controllers/BadgeController.java
@@ -6,7 +6,6 @@ import com.spring.badges.entities.Badge;
 import com.spring.badges.exceptions.BadgeNotFoundException;
 import com.spring.badges.services.BadgeService;
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,53 +16,42 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/badges")
-@Slf4j
 public class BadgeController {
     @Autowired
     private BadgeService badgeService;
 
     @GetMapping
     public ResponseEntity<List<Badge>> getBadges(@RequestParam(value = "id-person", required = false) String idPerson) {
-        log.info("BADGES - start : getBadges");
         List<Badge> badges = badgeService.getBadges(idPerson);
-        log.info("BADGES - stop : getBadges");
         return ResponseEntity.ok(badges);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Badge> getBadge(@PathVariable String id) throws BadgeNotFoundException {
-        log.info("BADGES - start : getBadge");
         Badge badge = badgeService.getBadge(id);
-        log.info("BADGES - stop : getBadge");
         return ResponseEntity.ok(badge);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Badge> updateCourse(@PathVariable String id, @Valid @RequestBody BadgeUpdateDto badge)
             throws BadgeNotFoundException {
-        log.info("BADGES - start : updateCourse");
         Badge updated = badgeService.updateBadge(id, badge);
-        log.info("BADGES - stop : updateCourse");
         return ResponseEntity.ok(updated);
     }
 
     @PostMapping
     public ResponseEntity<Badge> createBadge(@Valid @RequestBody BadgeCreationDto badge) {
-        log.info("BADGES - start : createBadge");
         Badge created = badgeService.createBadge(badge);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(created.getId())
                 .toUri();
-        log.info("BADGES - stop : createBadge");
         return ResponseEntity.created(location).body(created);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteBadge(@PathVariable String id) throws BadgeNotFoundException {
-        log.info("BADGES - start : deleteBadge");
         badgeService.deleteBadge(id);
-        log.info("BADGES - stop : deleteBadge");
         return ResponseEntity.ok("Deleted");
     }
 }
